f2878112f4abbca770cc1e3292a74ad083d9286e
MXZ23 <mikezhang23@gmail.com>
2026-02-03 09:51:16 -0800

    Update indentation again

--- pages/4_🧪_Amenity_Impact.py
+++ pages/4_🧪_Amenity_Impact.py
@@ -133,8 +133,8 @@ if len(amenity_impacts) > 0:
     with leg_col4:
         st.markdown("🔵 **Inconclusive**")
     
-    # Explanation for negative impacts
-    if len(not_recommended) > 0 or len(chart_data[chart_data['revenue_impact'] < 0]) > 0:
+# Explanation for negative impacts
+if len(not_recommended) > 0 or len(chart_data[chart_data['revenue_impact'] < 0]) > 0:
     with st.expander("Why do some amenities show negative or inconclusive results?"):
         st.markdown("""
         **Negative results don't mean the amenity hurts revenue.**
